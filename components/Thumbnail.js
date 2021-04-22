278a9b87a307133ba82e539c07ce6a87374582f0
Sahil Gupta <sahilgupta.312000@gmail.com>
2021-04-22 17:01:49 +0530

    Added FlipMove to the website

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,9 +1,14 @@
 import Image from 'next/image';
 import { ThumbUpIcon } from '@heroicons/react/outline';
-function Thumbnail({ key, result }) {
+import { forwardRef } from 'react';
+
+const Thumbnail = forwardRef(({ result }, ref) => {
 	const BASE_URL = 'https://image.tmdb.org/t/p/original/';
 	return (
-		<div className="group cursor-pointer p-2 transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
+		<div
+			ref={ref}
+			className="group cursor-pointer p-2 transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50"
+		>
 			<Image
 				layout="responsive"
 				src={
@@ -29,6 +34,6 @@ function Thumbnail({ key, result }) {
 			</div>
 		</div>
 	);
-}
+});
 
 export default Thumbnail;
