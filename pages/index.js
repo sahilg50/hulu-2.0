188a6ef049d3759b61ec7c858189eb2b92c73370
Sahil Gupta <sahilgupta.312000@gmail.com>
2021-04-22 16:47:41 +0530

    Done with the Results and Thumbnails component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Head from 'next/head';
 import Header from '../components/Header';
-
-export default function Home() {
+import Nav from '../components/Nav';
+import Results from '../components/Results';
+import requests from '../utils/requests';
+export default function Home({ results }) {
 	return (
 		<div>
 			<Head>
@@ -9,10 +11,23 @@ export default function Home() {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
-			{/* Header */}
 			<Header />
-			{/* Navbar */}
-			{/* Results */}
+			<Nav />
+			<Results results={results} />
 		</div>
 	);
+}
+
+export async function getServerSideProps(context) {
+	const genre = context.query.genre;
+	const request = await fetch(
+		`https://api.themoviedb.org/3${
+			requests[genre]?.url || requests.fetchTrending.url
+		}`
+	).then((res) => res.json());
+	return {
+		props: {
+			results: request.results,
+		},
+	};
 }
